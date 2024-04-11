08c4858742f750a8788132cc2f25ea775ee663a7
Taylor Ng <taylormng95@gmail.com>
2024-04-11 17:04:43 -0400

    adds correct scrolling for one element

--- js/portfolio_script.js
+++ js/portfolio_script.js
@@ -14,53 +14,34 @@ document.querySelector('#scrollBtn4').addEventListener('click', function(){
     scrollDiagonally('up-left');
 })
 
-const originalPosition = {x: 0, y: 0}
-let newPosition = {x: 0, y: 0};
+let currentPosition = {x: 0, y: 0};
 
 function scrollDiagonally(direction) {
-    const contentDivs = document.querySelectorAll('.scroll-content');
-    let currentPosition;
-    let counter = 0;
+    const content = document.querySelector('#sc-1');
     let transformValue;
+ 
+    switch(direction) {
+        case 'down-right':
+          currentPosition.x -= 100;
+          currentPosition.y -= 100;
+          break;
+        case 'down-left':
+          currentPosition.x += 100;
+          currentPosition.y -= 100;
+          break;
+        case 'up-right':
+          currentPosition.x -= 100;
+          currentPosition.y += 100;
+          break;
+        case 'up-left':
+          currentPosition.x += 100;
+          currentPosition.y += 100;
+          break;
+        default:
+          currentPosition.x = 0;
+          currentPosition.y = 0;
+    }
 
-    contentDivs.forEach(cdiv => {
-      console.log(cdiv);
-      counter++;
-
-      if(originalPosition === newPosition){
-        currentPosition = {x: 0, y: 0};
-      } else {currentPosition = newPosition}
-
-      switch(direction) {
-          case 'down-right':
-            currentPosition.x -= 100;
-            currentPosition.y -= 100;
-            break;
-          case 'down-left':
-            currentPosition.x += 100;
-            currentPosition.y -= 100;
-            break;
-          case 'up-right':
-            currentPosition.x -= 100;
-            currentPosition.y += 100;
-            break;
-          case 'up-left':
-            currentPosition.x += 100;
-            currentPosition.y += 100;
-            break;
-          default:
-            currentPosition.x = 0;
-            currentPosition.y = 0;
-      }
-
-      transformValue = `translate(${currentPosition.x}%, ${currentPosition.y}%)`;
-      cdiv.style.transform = transformValue;
-
-      if(originalPosition === newPosition && counter !== contentDivs.length){
-        currentPosition = {x: 0, y: 0};
-      } else {currentPosition = newPosition} // This happens at the end
-
-    });
-    newPosition.x = currentPosition.x;
-    newPosition.y = currentPosition.y;
+    transformValue = `translate(${currentPosition.x}%, ${currentPosition.y}%)`;
+    content.style.transform = transformValue;
 }
